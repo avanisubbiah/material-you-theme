625e77ad05c36f7d4a28d3435207169af2d14b59
ardjuh <63558466+ardjuh@users.noreply.github.com>
2022-08-06 21:55:13 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make && make install
-rm -r ~/.local/share/gnome-shell/extensions/material-you-theme@asubbiah.com/*
-cp 'material-you-theme@asubbiah.com.shell-extension.zip' ~/.local/share/gnome-shell/extensions/material-you-theme@asubbiah.com
-rm 'material-you-theme@asubbiah.com.shell-extension.zip'
-cd ~/.local/share/gnome-shell/extensions/material-you-theme@asubbiah.com
-unzip 'material-you-theme@asubbiah.com.shell-extension.zip'
-rm 'material-you-theme@asubbiah.com.shell-extension.zip'
