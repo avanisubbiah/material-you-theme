35aef72f4c168d9419789e2909a610886ba07f26
Daniel Sánchez <dev@daniromo.me>
2022-09-15 18:04:12 -0500

    Run wal on background change and use system sassc

--- material-you-theme@asubbiah.com/utils/ext_utils.js
+++ material-you-theme@asubbiah.com/utils/ext_utils.js
@@ -3,7 +3,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const EXTENSIONDIR = Me.dir.get_path();
 
-function check_npm() {
-    const file = Gio.File.new_for_path(EXTENSIONDIR + "/node_modules/sass/sass.js");
+function check_bin(path) {
+    const file = Gio.File.new_for_path(path);
     return file.query_exists(null);
 }
