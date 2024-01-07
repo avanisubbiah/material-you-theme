65c2283c0d7be37ef0108b88ee4b4c6c03356604
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-01-07 13:20:20 +0100

    Fix ext_utils.check_wal is not a function
    Solve issue #82

--- material-you-theme@asubbiah.com/utils/ext_utils.js
+++ material-you-theme@asubbiah.com/utils/ext_utils.js
@@ -5,3 +5,12 @@ export function check_npm(extensiondir) {
     const file = Gio.File.new_for_path(extensiondir + "/node_modules/sass/sass.js");
     return file.query_exists(null);
 }
+
+export function check_wal() {
+  const process = Gio.Subprocess.new(
+    ["wal", "-v"],
+    Gio.SubprocessFlags.NONE
+  );
+  process.wait(null);
+  return process.get_successful();
+}
